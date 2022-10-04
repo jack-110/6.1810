0d7569eb770e9e9fa8bad1b746fd55f27bf1c583
cy <13990779978@163.com>
2022-10-04 17:25:53 +0800

    my solution for lab traps backtrace.

--- kernel/printf.c
+++ kernel/printf.c
@@ -122,6 +122,7 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -132,4 +133,31 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void 
+printra(uint64 fp, uint64 prepg)
+{
+  uint64 curpg = PGROUNDDOWN(fp);
+  if(prepg != curpg)
+    return;
+  
+  uint64 *ra = (uint64 *)(fp - 8);
+  printf("%p\n", *ra);
+
+  uint64 *prefp = (uint64 *)(fp - 16);
+  printra(*prefp, curpg);
+}
+
+void
+backtrace(void)
+{
+  uint64 fp = r_fp();
+  uint64 pgnumber = PGROUNDDOWN(fp);
+
+  uint64 *ra = (uint64 *) (fp - 8);
+  printf("%p\n", *ra);
+
+  uint64 *prefp = (uint64 *) (fp - 16); 
+  printra(*prefp, pgnumber);
 }
