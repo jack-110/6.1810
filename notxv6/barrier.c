fe22dd3136d6dfcb6b581514e5ff3b2da4037541
cy <13990779978@163.com>
2022-11-13 23:07:33 +0800

    my solution for using barrier.

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -10,7 +10,7 @@ static int round = 0;
 struct barrier {
   pthread_mutex_t barrier_mutex;
   pthread_cond_t barrier_cond;
-  int nthread;      // Number of threads that have reached this round of the barrier
+  int nthread;    // Number of threads that have reached this round of the barrier
   int round;     // Barrier round
 } bstate;
 
@@ -25,12 +25,20 @@ barrier_init(void)
 static void 
 barrier()
 {
-  // YOUR CODE HERE
-  //
-  // Block until all threads have called barrier() and
-  // then increment bstate.round.
-  //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  int cround = bstate.round; // current round
+  bstate.nthread++;
+  if (bstate.nthread == nthread) {
+    bstate.round++;
+    bstate.nthread = 0;
+    pthread_mutex_unlock(&bstate.barrier_mutex);
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  } else {
+    while (cround == bstate.round) {
+      pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    }
+    pthread_mutex_unlock(&bstate.barrier_mutex);
+  }
 }
 
 static void *
